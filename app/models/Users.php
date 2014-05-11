94231370d6dbe24fd52e2fb6336d734587737540
boonnuang <boonnuang@gmail.com>
2014-05-11 16:07:40 +0700

    Add models file Users.php By Boonnuang

---
+++ app/models/Users.php
@@ -0,0 +1,21 @@
+<?php
+class Users extends \Phalcon\Mvc\Model
+{
+	/*
+public function getItems(){
+	$items = array(
+			"name 1",
+			"name 2",
+			"name 3"
+);
+return $items;
+}
+
+public function getMonth(){
+	$months = array("อาทิตย์","จันทร์","อังคาร");
+	return $months;
+}
+*/
+	public $id;
+	public $name;
+}
